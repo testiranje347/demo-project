9257c0bc980474c646ea3c83d3c4bfd090abc88d
ancovic <antcov0101@gmai.com>
2025-03-17 18:43:55 +0100

    opet nekakav commit

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("New line to showcase git commit.");
+        Console.WriteLine("Another line to showcase commit");
     }
 }
